a521490a8ab6d44aced58e4cc6d56ee12d291d13
adityaa389 <aditya.bedare24@pcu.edu.in>
2025-01-31 12:44:00 +0530

    Update config.js

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+ require('dotenv').config();
 const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
 const { Pool } = require('pg');
 
